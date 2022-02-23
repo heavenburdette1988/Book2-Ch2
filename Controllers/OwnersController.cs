204fe9f99f182b561b865a9bdd7311390cb2f887
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-02-23 13:42:46 -0500

    delete is finished

--- Controllers/OwnersController.cs
+++ Controllers/OwnersController.cs
@@ -84,22 +84,27 @@ namespace DogGoMVC2.Controllers
         // GET: OwnerController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            return View(owner);
         }
 
         // POST: OwnerController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Owner owner)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                _ownerRepo.DeleteOwner(id);
+
+                return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                return View(owner);
             }
         }
     }
-}
+    }
+
