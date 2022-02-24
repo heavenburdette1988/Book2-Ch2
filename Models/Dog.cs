374d3ab34b754812c8ecb47254d991fadaaac2eb
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-02-24 13:17:31 -0500

    need to add dog

---
+++ Models/Dog.cs
@@ -0,0 +1,11 @@
+﻿namespace DogGoMVC2.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string
+        
+    }
+}
---
+++ Models/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DogGoMVC2.Models
+{
+    public class ProfileViewModel
+    {
+        //public class ProfileViewModel
+        //{
+        //    public Owner Owner { get; set; }
+        //    public List<Walker> Walkers { get; set; }
+        //    public List<Dog> Dogs { get; set; }
+        //}
+    }
+}
